0bed94797a030358ff7718513d4335d9f990fd4c
Alexander Perez <alexcyberay@gmail.com>
2022-07-24 20:03:49 -0400

    add model to table with sequelize

--- DBConfig.js
+++ DBConfig.js
@@ -26,7 +26,9 @@ const sequelize = new handSequelize(env.database, env.username, env.password, {
 
 // authentication postgres DB
 sequelize.authenticate().then(() => {
+    console.log("*************************************")
     console.log("Success conect Data Bases postgres!!!!")
+    console.log("*************************************")
   }).catch((err) => {
     console.log(err)
 });
@@ -37,11 +39,23 @@ db.handSequelize = handSequelize
 db.sequelize = sequelize
 
 // objets to tables
-db.user = require('./src/models/client.model')(sequelize, handSequelize)
+db.client = require('./src/models/client.model')(sequelize, handSequelize)
 db.room = require('./src/models/room.model')(sequelize, handSequelize)
 db.reserve = require('./src/models/reserve.model')(sequelize, handSequelize)
 db.payment = require('./src/models/payment.model')(sequelize, handSequelize)
 db.factura = require('./src/models/factura.model')(sequelize, handSequelize)
-// realtionship
+// realtionship one to many or many to one
+db.client.hasMany(db.reserve, { onDelete: 'CASCADE'});
+db.reserve.belongsTo(db.client, { onDelete: 'CASCADE' });
+
+db.room.hasMany(db.reserve, { onDelete: 'CASCADE'});
+db.reserve.belongsTo(db.room, { onDelete: 'CASCADE' });
+
+db.reserve.hasMany(db.payment, { onDelete: 'CASCADE'});
+db.payment.belongsTo(db.reserve, { onDelete: 'CASCADE' });
+
+db.factura.hasMany(db.payment, { onDelete: 'CASCADE'});
+db.payment.belongsTo(db.factura, { onDelete: 'CASCADE' });
+// relationship one to one
 
 module.exports = db;
